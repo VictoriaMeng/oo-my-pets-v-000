8a0739ea7d01f87ec73e1cf8cf78b19953f60333
Victoria Meng <nights1ststar@protonmail.com>
2018-03-25 22:51:37 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -51,7 +51,7 @@ class Owner
   end
 
   def sell_pets
-    pets.each do |species, list|
+    pets.each do |species, _|
       pets[species].each do |pet|
         pet.mood = "nervous"
       end
@@ -61,6 +61,7 @@ class Owner
   end
 
   def list_pets
+    
   end
 
 end
