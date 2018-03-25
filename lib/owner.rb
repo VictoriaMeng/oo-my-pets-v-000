565b856ec7ac3a20c5abf496eb237c4ca6aa7ee0
Victoria Meng <nights1ststar@protonmail.com>
2018-03-25 22:39:36 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -43,6 +43,11 @@ class Owner
   end
 
   def play_with_cats
+    pets[:cats].each { |cat| cat.mood = "happy" }
+  end
+
+  def feed_fish
+    pets[:fishes].each { |fish| fish.mood = "happy" }
   end
 
 end
